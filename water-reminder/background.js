222f3a415d760b2ac558ad7a8d4f9365ab014d7d
Rishabh Jain <mailrishabh1403@gmail.com>
2020-04-01 13:17:20 +0530

    water reminder - add notifications

--- water-reminder/background.js
+++ water-reminder/background.js
@@ -1,1 +1,21 @@
 console.log("in background script")
+
+let defaultDuration = 1.0;
+
+chrome.alarms.onAlarm.addListener(function (alarm){
+  console.log(alarm)
+  chrome.notifications.create("my notification", {
+    type: "basic",
+    iconUrl: "./icons/32.png",
+    title: "Drink Water",
+    "message" : "Dumy notifications"
+  }, function (notificationID){
+    console.log("displayed the notification")
+  })
+});
+
+function createAlarm(){
+  chrome.alarms.create("drink water", {delayInMinutes : defaultDuration});
+}
+
+createAlarm()
