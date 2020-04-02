762851538274f0561676ab88a154f9cc4a7797a5
Rishabh Jain <mailrishabh1403@gmail.com>
2020-04-02 11:26:34 +0530

    stop netflix video 2

--- stop-netflix/content.js
+++ stop-netflix/content.js
@@ -1,1 +1,11 @@
-document.write("Not Allowed!!!")
+
+
+// if the day is saunday, then we'll Allowed
+// else blocked
+
+const date = new Date();
+if(date.getDay() === 4){
+  alert("Allowed to visit");
+}else{
+  document.write("You shall not pass!!!")
+}
