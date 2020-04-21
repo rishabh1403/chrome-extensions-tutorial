150a3ca10ae89ff989f6e390e72994f8c18944a0
Rishabh Jain <mailrishabh1403@gmail.com>
2020-04-21 13:33:59 +0530

    add weather api

--- momentum-clone/weather.js
+++ momentum-clone/weather.js
@@ -3,7 +3,7 @@ $(function(){
   // if yes, we will ask browser for location
   // call api with location details and get the weather data
 
-  function getLocation(){
+  function getLocation(callback){
     if(!navigator.geolocation){
       console.log("Geolocation not supported by browser")
       return
@@ -12,7 +12,7 @@ $(function(){
     function success(position){
       const lat = position.coords.latitude;
       const long = position.coords.longitude;
-      console.log(lat, long)
+      callback(lat, long)
     }
 
     function error(){
@@ -21,5 +21,24 @@ $(function(){
     navigator.geolocation.getCurrentPosition(success, error)
   }
 
-  getLocation()
+  getLocation(function(lat,long){
+    const domain = `https://api.openweathermap.org/data/2.5/onecall`
+    const apiKey = `c2f90a8e060cf9d5b4a27887f9984422`
+
+    const api = `${domain}?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`
+
+    fetch(api)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        console.log(data)
+        console.log(data.current.temp)
+        console.log(data.current.weather[0].icon)
+        $("#weather").text(data.current.temp)
+        const imageUrl = `http://openweathermap.org/img/wn/`
+        $("#weather-icon").attr("src", `${imageUrl}${data.current.weather[0].icon}.png`)
+        // 
+      })
+  })
 })
